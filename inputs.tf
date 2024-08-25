065175225954a4e8dd9e3dc604359fd7f0a270a5
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-25 19:03:00 +0530

    Added the description for input and output

--- inputs.tf
+++ inputs.tf
@@ -1,19 +1,24 @@
 variable "region" {
   type    = string
+  description = "selects the region to the default value set in the users AWS cli Config"
 }
 
 variable "image" {
   type    = list(string)
+  description = "List of AMIs with EC2 instances gets created"
 }
 
 variable "instancetype" {
   type    = list(string)
+  description = "List of Instance types should be provided by user"
 }
 
 variable "servers" {
   type    = number
+  description = "Number of servers to be created"
 }
 
 variable "port_and_protocol" {
   type = map(any)
+  description = "Provide the ingress rules with map. Eg {'HTTP':'8080', 'HTTPS':'443'}"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "instance_ids" {
   value = aws_instance.ec2_instance[*].id
+  description = "This output holds the list of EC2 instance IDs"
 }
 
 output "custom_sg_id" {
   value = aws_security_group.custom_sg.id
+  description = "This output holds the security group ID"
 }
 
 output "custom_sg_name" {
   value = aws_security_group.custom_sg.name
+  description = "This output holds the security group name"
 }
